36bbaa1d6c8c7ebec45078b6e715205224bc81d7
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-04-12 15:56:47 -0300

    entrega requisito 5

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: 'normal',
-      cardTrunfo: 'false',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    /* this.onSaveButtonClick = this.onSaveButtonClick.bind(this); */
   }
 
   onInputChange({ target }) {
@@ -23,13 +25,49 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
+      const {
+        cardName, cardDescription, cardAttr1, cardAttr2,
+        cardAttr3, cardImage,
+      } = this.state;
+      const somaAtributos = parseInt(cardAttr1, 10)
+    + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10);
+      const maxValue = 210;
+      const max = 90;
+      const min = 0;
+      if (
+        cardName.length > 0
+      && cardDescription.length > 0
+      && parseInt(cardAttr1, 10) >= min
+      && parseInt(cardAttr1, 10) <= max
+      && parseInt(cardAttr2, 10) >= min
+      && parseInt(cardAttr2, 10) <= max
+      && parseInt(cardAttr3, 10) >= min
+      && parseInt(cardAttr3, 10) <= max
+      && somaAtributos <= maxValue
+      && cardImage.length > 0
+      ) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
     });
+  }
+
+  onSaveButtonClick() {
+
   }
 
   render() {
     const {
       cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -44,6 +82,8 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           cardName={ cardName }
